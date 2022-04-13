8bc1ec0393f13c32cda26b8dc1977191efc4e5f4
Nikos Leonov <n_leonov@protonmail.com>
2022-04-14 01:07:52 +0300

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by macbook on 14/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
